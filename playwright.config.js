120493f3b6455009e33cd53ae20d45fea55c2e3d
preetiVijay <vijaywargiya.preeti@gmail.com>
2025-08-17 00:02:01 +0200

    Updated allure report config

--- playwright.config.js
+++ playwright.config.js
@@ -29,7 +29,10 @@ export default defineConfig({
   // workers: process.env.CI ? 1 : undefined,
   workers: 2,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
